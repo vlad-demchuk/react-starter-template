990e9f196f333155cd7b5abd94e79bcb71c3ff78
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2025-05-08 20:32:13 +0300

    Initialize Vite React TS project files with directories according to feature-based structure, add structure-guide.md with detailed structure description

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+
+/**
+ * Main application component
+ */
+const App = () => {
+  return (
+    <>Vite + React + TS Starter template</>
+  );
+};
+
+export default App;
